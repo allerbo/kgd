0bee42efbecd38a680c377539ca4640cd289e207
Oskar Allerbo <allerbo@chalmers.se>
2023-03-01 19:06:53 +0100

    sqrt_freq2

--- bs.py
+++ bs.py
@@ -7,6 +7,7 @@ from help_fcts import r2, krr, kern, gcv, log_marg, log_marg_ns
 
 
 
+#np.random.seed(0)
 data=pd.read_csv('bs_2000.csv',sep=',').to_numpy()
 #max_obs=0
 #for d in np.unique(data[:,1]):
@@ -22,54 +23,74 @@ data=pd.read_csv('bs_2000.csv',sep=',').to_numpy()
 #data=data[data[:,1]==1]
 #data=data[data[:,1]==2]
 #data=data[data[:,1]==3]
-#for day in np.unique(data[:,1]):
-for day in [21]:
+#for day in [21]:
+LBDA_MIN=1e-4
+LBDA_MAX=10
+SIGMA_MIN=1e-2
+SIGMA_MAX=10
+SWEEP=False
+if SWEEP:
+  days=np.unique(data[:,1])
+  #np.random.shuffle(days)
+else:
+  days=[1]
+for day in days:
   data1=data[data[:,1]==day]
-  
+  np.random.shuffle(data1)
+  X=data1[:,8:10]
+  X=(X-np.mean(X, 0))/np.std(X,0)
+  #y=data1[:,5].reshape((-1,1))
+  y=data1[:,7].reshape((-1,1))
+  n=X.shape[0]
+  per=np.random.permutation(n)
+  folds=np.array_split(per,5)
+   
   r2s_kgd=[]
   r2s_krr_cv=[]
   r2s_krr_lm=[]
   r2s_krr_lm_ns=[]
-  for seed in range(10):
-    np.random.seed(seed)
-    np.random.shuffle(data1)
+  #for seed in range(10):
+  for v_fold in range(len(folds)):
+    t_idxs=np.concatenate([folds[t_fold] for t_fold in range(len(folds)) if v_fold != t_fold])
+    v_idxs=folds[v_fold]
+
+    X_tr=X[t_idxs,:]
+    X_val=X[v_idxs,:]
+    y_tr=y[t_idxs,:]
+    y_val=y[v_idxs,:]
+    n_tr=X_tr.shape[0]
     
-    X=data1[:,8:10]
-    y=data1[:,7].reshape((-1,1))
-    
-    n_tot=data1.shape[0]
-    n_tr=round(0.7*n_tot)
-    
-    X_tr=X[:n_tr,:]
-    X_val=X[n_tr:,:]
-    y_tr=y[:n_tr,:]
-    y_val=y[n_tr:,:]
-    
-    y1_kgd=kgd(X,X_tr,y_tr, plot=False, step_size=0.01)
+    y1_kgd=kgd(np.vstack((X_tr,X_val)),X_tr,y_tr, plot=False, step_size=0.01)
     r2s_kgd.append(r2(y_val,y1_kgd[n_tr:,:]))
     
-    #lbda, sigma=log_marg_ns(X_tr,y_tr, lbda_bounds=[1,1e5],sigma_bounds=[1,1e3])
-  
-    lbda_cv, sigma_cv=gcv(X_tr,y_tr, np.geomspace(1e-3,1,30),np.geomspace(1,100,30))
-    lbda_lm, sigma_lm=log_marg(X_tr,y_tr, lbda_bounds=[1e-3,10],sigma_bounds=[1,100], lbda_seed=lbda_cv, sigma_seed=sigma_cv)
-    lbda_lm_ns, sigma_lm_ns=log_marg_ns(X_tr,y_tr, lbda_bounds=[1e-3,10],sigma_bounds=[1,100])
-    y1_krr_cv=krr(X,X_tr,y_tr,lbda_cv,sigma_cv)
-    y1_krr_lm=krr(X,X_tr,y_tr,lbda_lm,sigma_lm)
-    y1_krr_lm_ns=krr(X,X_tr,y_tr,lbda_lm_ns,sigma_lm_ns)
+    lbda_cv, sigma_cv=gcv(X_tr,y_tr, np.geomspace(LBDA_MIN, LBDA_MAX,30),np.geomspace(SIGMA_MIN, SIGMA_MAX, 30))
+    if lbda_cv in [LBDA_MIN, LBDA_MAX] or sigma_cv in [SIGMA_MIN, SIGMA_MAX]:
+      print('OOOBS!',day, lbda_cv, sigma_cv)
+    #lbda_cv, sigma_cv=gcv(X_tr,y_tr, np.geomspace(1e-3,1,30),np.geomspace(1,100,30))
+    #lbda_lm, sigma_lm=log_marg(X_tr,y_tr, lbda_bounds=[0.1*LBDA_MIN, 10*LBDA_MAX],sigma_bounds=[0.1*SIGMA_MIN, 10*SIGMA_MAX], lbda_seed=lbda_cv, sigma_seed=sigma_cv)
+    #lbda_lm_ns, sigma_lm_ns=log_marg_ns(X_tr,y_tr, lbda_bounds=[0.1*LBDA_MIN, 10*LBDA_MAX],sigma_bounds=[0.1*SIGMA_MIN, 10*SIGMA_MAX])
+    y1_krr_cv=krr(np.vstack((X_tr, X_val)),X_tr,y_tr,lbda_cv,sigma_cv)
+    #y1_krr_lm=krr(np.vstack((X_tr, X_val)),X_tr,y_tr,lbda_lm,sigma_lm)
+    #y1_krr_lm_ns=krr(np.vstack((X_tr, X_val)),X_tr,y_tr,lbda_lm_ns,sigma_lm_ns)
     r2s_krr_cv.append(r2(y_val,y1_krr_cv[n_tr:,:]))
-    r2s_krr_lm.append(r2(y_val,y1_krr_lm[n_tr:,:]))
-    r2s_krr_lm_ns.append(r2(y_val,y1_krr_lm_ns[n_tr:,:]))
+    #r2s_krr_lm.append(r2(y_val,y1_krr_lm[n_tr:,:]))
+    #r2s_krr_lm_ns.append(r2(y_val,y1_krr_lm_ns[n_tr:,:]))
     p_val_cv=wilcoxon(r2s_kgd, r2s_krr_cv, alternative='greater')[1]
-    p_val_lm=wilcoxon(r2s_kgd, r2s_krr_lm, alternative='greater')[1]
-    p_val_lm_ns=wilcoxon(r2s_kgd, r2s_krr_lm_ns, alternative='greater')[1]
+    #p_val_lm=wilcoxon(r2s_kgd, r2s_krr_lm, alternative='greater')[1]
+    #p_val_lm_ns=wilcoxon(r2s_kgd, r2s_krr_lm_ns, alternative='greater')[1]
+    p_val_vc=wilcoxon(r2s_krr_cv, r2s_kgd, alternative='greater')[1]
     
-    print(f'KGD:      {np.quantile(r2s_kgd,0.1):.2f}, {np.median(r2s_kgd):.2f}, {np.quantile(r2s_kgd,0.9):.2f}.')
-    print(f'KRR CV:   {np.quantile(r2s_krr_cv,0.1):.2f}, {np.median(r2s_krr_cv):.2f}, {np.quantile(r2s_krr_cv,0.9):.2f}. Lambda: {lbda_cv:.3g}, Sigma: {sigma_cv:.3g}.')
-    print(f'KRR LM:   {np.quantile(r2s_krr_lm,0.1):.2f}, {np.median(r2s_krr_lm):.2f}, {np.quantile(r2s_krr_lm,0.9):.2f}. Lambda: {lbda_lm:.3g}, Sigma: {sigma_lm:.3g}.')
-    print(f'KRR LMNS: {np.quantile(r2s_krr_lm_ns,0.1):.2f}, {np.median(r2s_krr_lm_ns):.2f}, {np.quantile(r2s_krr_lm_ns,0.9):.2f}. Lambda: {lbda_lm_ns:.3g}, Sigma: {sigma_lm_ns:.3g}.')
-    print(f'p cv:     {p_val_cv:.2f}.')
-    print(f'p lm:     {p_val_lm:.2f}.')
-    print(f'p lmns:   {p_val_lm_ns:.2f}.')
-    print('')
-  print(day, p_val_cv)
+    if not SWEEP:
+      print(f'KGD:      {np.quantile(r2s_kgd,0.1):.2f}, {np.median(r2s_kgd):.2f}, {np.quantile(r2s_kgd,0.9):.2f}.')
+      print(f'KRR CV:   {np.quantile(r2s_krr_cv,0.1):.2f}, {np.median(r2s_krr_cv):.2f}, {np.quantile(r2s_krr_cv,0.9):.2f}. Lambda: {lbda_cv:.3g}, Sigma: {sigma_cv:.3g}.')
+      #print(f'KRR LM:   {np.quantile(r2s_krr_lm,0.1):.2f}, {np.median(r2s_krr_lm):.2f}, {np.quantile(r2s_krr_lm,0.9):.2f}. Lambda: {lbda_lm:.3g}, Sigma: {sigma_lm:.3g}.')
+      #print(f'KRR LMNS: {np.quantile(r2s_krr_lm_ns,0.1):.2f}, {np.median(r2s_krr_lm_ns):.2f}, {np.quantile(r2s_krr_lm_ns,0.9):.2f}. Lambda: {lbda_lm_ns:.3g}, Sigma: {sigma_lm_ns:.3g}.')
+      print(f'p cv:     {p_val_cv:.2f}.')
+      #print(f'p lm:     {p_val_lm:.2f}.')
+      #print(f'p lmns:   {p_val_lm_ns:.2f}.')
+      #print(np.mean(y), np.std(y))
+      print('')
+  if SWEEP:
+    #print(day, p_val_cv, p_val_lm, p_val_lm_ns, np.mean(y), np.std(y))
+    print(day, np.round(p_val_cv,3), np.round(p_val_vc,3))
   
---
+++ sqrt_freq2.py
@@ -0,0 +1,109 @@
+import numpy as np
+from kgd import kgd
+from matplotlib import pyplot as plt
+from kgd import kgd
+from scipy.optimize import minimize
+from scipy.stats import wilcoxon
+from help_fcts import r2, krr, kern, gcv, log_marg, log_marg_ns
+import sys
+
+NS=500
+
+def make_data(seed):
+  np.random.seed(seed)
+  N_TR=100
+  def fy(x):
+    #return np.sin(2*np.pi*np.sqrt((x+1)))
+    #return np.sin(2*np.pi*(x+1)**0.3)
+    return np.sin(2*np.pi*np.sign(x)*np.abs(x)**(1/3))
+  #x_tr=np.random.exponential(10,(N_TR,1))
+  x_tr=np.random.normal(0,50,(N_TR,1))
+  y_tr=fy(x_tr)+np.random.normal(0,.2,x_tr.shape)
+  x_val=np.random.uniform(np.min(x_tr), np.max(x_tr), NS).reshape((-1,1))
+  y_val=fy(x_val)
+  #x1=np.linspace(np.min(x_tr), np.max(x_tr), 1000).reshape((-1,1))
+  x1=np.linspace(-5**3, 5**3, 1000).reshape((-1,1))
+  y1=fy(x1)
+  lbda_bounds=[1e-4,.1]
+  sigma_bounds=[1e-2,100]
+  return x_tr, y_tr, x_val, y_val, x1, y1, lbda_bounds, sigma_bounds
+
+def shift_root(x,power,shift):
+  return np.sign(x)*((np.abs(x)+shift)**power-shift**power)
+  
+def make_data(seed=None):
+  if not seed is None:
+    np.random.seed(seed)
+  N_TR=100
+  def fy(x):
+    #return np.sin(4*2*np.pi*np.sign(x)*np.abs(x)**(1/4))
+    #return np.sin(4*2*np.pi*shift_root(x,1/4,0))
+    return np.sin(10*2*np.pi*shift_root(x,1/4,0))
+  x_tr=np.random.normal(0,1,(N_TR,1))
+  y_tr=fy(x_tr)+np.random.normal(0,.2,x_tr.shape)
+  x_val=np.random.uniform(np.min(x_tr), np.max(x_tr), NS).reshape((-1,1))
+  #x_val=np.linspace(np.min(x_tr), np.max(x_tr), NS).reshape((-1,1))
+  y_val=fy(x_val)
+  x1=np.linspace(-3, 3, 1000).reshape((-1,1))
+  y1=fy(x1)
+  lbda_bounds=[1e-3,1]
+  sigma_bounds=[1e-2,1]
+  return x_tr, y_tr, x_val, y_val, x1, y1, lbda_bounds, sigma_bounds
+
+#x_tr=sample(1000)
+#fig,ax=plt.subplots(1,1,figsize=(20,6))
+#ax.hist(x_tr,50)
+#fig.savefig('figures/sqrt_freq.pdf')
+#sys.exit()
+
+r2s_kgd=[]
+r2s_krr_cv=[]
+r2s_krr_lm=[]
+r2s_krr_lm_ns=[]
+for seed in range(100):
+  print(seed)
+  #x_tr, y_tr, x_val, y_val, x1,y1, lbda_bounds, sigma_bounds=make_data(seed)
+  x_tr, y_tr, x_val, y_val, x1,y1, lbda_bounds, sigma_bounds=make_data()
+  n_tr=x_tr.shape[0]
+  xs=np.vstack((x_tr,x_val))
+  xs_argsort=xs.argsort(0)
+  
+  y1_kgd=kgd(xs,x_tr,y_tr, plot=False, step_size=0.01, sleep_time=0.1,val_data=[x_val, y_val])
+  r2s_kgd.append(r2(y_val,y1_kgd[n_tr:,:]))
+  
+  lbda_cv, sigma_cv=gcv(x_tr,y_tr, lbda_bounds, sigma_bounds)
+  y1_krr_cv=krr(xs,x_tr,y_tr,lbda_cv,sigma_cv)
+  r2s_krr_cv.append(r2(y_val,y1_krr_cv[n_tr:,:]))
+  p_val_cv=wilcoxon(r2s_kgd, r2s_krr_cv, alternative='greater')[1]
+  
+  lbda_lm, sigma_lm=log_marg(x_tr,y_tr, [0.1*lbda_bounds[0], 10*lbda_bounds[1]],[0.1*sigma_bounds[0], 10*sigma_bounds[1]], lbda_seed=lbda_cv, sigma_seed=sigma_cv)
+  y1_krr_lm=krr(xs,x_tr,y_tr,lbda_lm,sigma_lm)
+  r2s_krr_lm.append(r2(y_val,y1_krr_lm[n_tr:,:]))
+  p_val_lm=wilcoxon(r2s_kgd, r2s_krr_lm, alternative='greater')[1]
+   
+  lbda_lm_ns, sigma_lm_ns=log_marg_ns(x_tr,y_tr, [0.1*lbda_bounds[0], 10*lbda_bounds[1]],[0.1*sigma_bounds[0], 10*sigma_bounds[1]])
+  y1_krr_lm_ns=krr(xs,x_tr,y_tr,lbda_lm_ns,sigma_lm_ns)
+  r2s_krr_lm_ns.append(r2(y_val,y1_krr_lm_ns[n_tr:,:]))
+  p_val_lm_ns=wilcoxon(r2s_kgd, r2s_krr_lm_ns, alternative='greater')[1]
+
+  print(f'KGD:      {np.quantile(r2s_kgd,0.1):.2f}, {np.median(r2s_kgd):.2f}, {np.quantile(r2s_kgd,0.9):.2f}.')
+  print(f'KRR CV:   {np.quantile(r2s_krr_cv,0.1):.2f}, {np.median(r2s_krr_cv):.2f}, {np.quantile(r2s_krr_cv,0.9):.2f}. Lambda: {lbda_cv:.3g}, Sigma: {sigma_cv:.3g}.')
+  print(f'KRR LM:   {np.quantile(r2s_krr_lm,0.1):.2f}, {np.median(r2s_krr_lm):.2f}, {np.quantile(r2s_krr_lm,0.9):.2f}. Lambda: {lbda_lm:.3g}, Sigma: {sigma_lm:.3g}.')
+  print(f'KRR LMNS: {np.quantile(r2s_krr_lm_ns,0.1):.2f}, {np.median(r2s_krr_lm_ns):.2f}, {np.quantile(r2s_krr_lm_ns,0.9):.2f}. Lambda: {lbda_lm_ns:.3g}, Sigma: {sigma_lm_ns:.3g}.')
+  print(f'p cv:     {p_val_cv:.2f}.')
+  print(f'p lm:     {p_val_lm:.2f}.')
+  print(f'p lmns:   {p_val_lm_ns:.2f}.')
+  print('')
+
+  
+  fig,ax=plt.subplots(1,1,figsize=(20,6))
+  ax.plot(x1,y1,'C7',lw=3)
+  ax.plot(x_tr,y_tr,'ok')
+  #ax.plot(x_val,y_val,'or')
+  ax.plot(xs[xs_argsort,0],y1_krr_cv[xs_argsort,0],'C0')
+  ax.plot(xs[xs_argsort,0],y1_krr_lm[xs_argsort,0],'C1')
+  ax.plot(xs[xs_argsort,0],y1_krr_lm_ns[xs_argsort,0],'C3:')
+  ax.plot(xs[xs_argsort,0],y1_kgd[xs_argsort,0],'C2')
+  fig.savefig('figures/sqrt_freq.pdf')
+  plt.close()
+
